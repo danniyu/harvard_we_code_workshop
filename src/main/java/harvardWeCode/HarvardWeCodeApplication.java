d90835602280c73a5d3ecb0c0a78075e9f774877
Molly Bartlett <molly.bartlett@thoughtworks.com>
2015-02-03 16:50:13 -0600

    Molly - initializing a spring boot project with gradle

---
+++ src/main/java/harvardWeCode/HarvardWeCodeApplication.java
@@ -0,0 +1,12 @@
+package harvardWeCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarvardWeCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarvardWeCodeApplication.class, args);
+    }
+}
---
+++ src/test/java/harvardWeCode/HarvardWeCodeApplicationTests.java
@@ -0,0 +1,16 @@
+package harvardWeCode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = HarvardWeCodeApplication.class)
+public class HarvardWeCodeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
