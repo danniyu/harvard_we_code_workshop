3b040fa682da109a59b24fc6158f3dfbd68c47cd
Molly Bartlett <molly.bartlett@thoughtworks.com>
2015-02-04 09:21:37 -0600

    Molly - updated parsing of the csv file

--- src/main/java/harvardWeCode/StudentService.java
+++ src/main/java/harvardWeCode/StudentService.java
@@ -11,33 +11,77 @@ import java.util.Map;
 
 public class StudentService {
     private final String intentionToMajor = String.format(Paths.get("").toAbsolutePath() + "/" + "intentions_of_freshman_to_major_in_stem.csv");
-    private Map something;
+    private Map something = new HashMap<>();
+    private String[] majors;
 
     public StudentService() {
         try {
             CSVReader reader = new CSVReader(new FileReader(intentionToMajor));
             List<String[]> strings = reader.readAll();
-            String[] majors = strings.get(0);
+            majors = strings.get(0);
 
-            for (String[] raceEthnicityGender : strings.subList(1, strings.size() - 1)) {
-                something = new HashMap<>();
-                List majorsPercentages = new ArrayList<>();
-
-                for(int i = 1; i < raceEthnicityGender.length; i++) {
-                    Map majorPercent = new HashMap<>();
-                    majorPercent.put(majors[i], raceEthnicityGender[i]);
-                    majorsPercentages.add(majorPercent);
-                }
-
-                something.put(raceEthnicityGender[0], majorsPercentages);
+            int min = 1;
+            int max = 4;
+            strings = filterOutEmptyRows(strings);
+            while(max < strings.size()) {
+                List<String[]> raceEthnicityAndGenderStatistics = strings.subList(min, max);
+                Map listOfMajorsByPercent = createListOfMajorsByPercent(raceEthnicityAndGenderStatistics);
+                something.putAll(listOfMajorsByPercent);
+                min = min + 3;
+                max = max + 3;
             }
+
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    private List<String[]> filterOutEmptyRows(List<String[]> strings) {
+        ArrayList<String[]> filteredStrings = new ArrayList<>();
+        for (String[] string : strings) {
+            boolean hasATitle = !string[0].trim().equals("");
+            if(hasATitle) {
+                filteredStrings.add(string);
+            }
+        }
+        return filteredStrings;
+    }
+
     public Map list() {
         return something;
+    }
+
+    public Map createListOfMajorsByPercent(List<String[]> exampleData) {
+        Map majorByPercent = new HashMap<>();
+        HashMap<Object, Object> genderRaceOrEthnicityAndPercentages = new HashMap<>();
+
+        for (String[] raceEthnicityOrGenderAndPercentOfMajor : exampleData) {
+            String title = raceEthnicityOrGenderAndPercentOfMajor[0];
+            ArrayList<String> femaleOrMale = new ArrayList<>();
+            femaleOrMale.add("Female");
+            femaleOrMale.add("Male");
+
+            if(femaleOrMale.contains(title)) {
+                genderRaceOrEthnicityAndPercentages.put(title, creatListOfMajorAndPercent(raceEthnicityOrGenderAndPercentOfMajor));
+            } else {
+                genderRaceOrEthnicityAndPercentages.put("Total", creatListOfMajorAndPercent(raceEthnicityOrGenderAndPercentOfMajor));
+            }
+        }
+
+        majorByPercent.put(exampleData.get(0)[0], genderRaceOrEthnicityAndPercentages);
+        return majorByPercent;
+    }
+
+    private List creatListOfMajorAndPercent(String[] raceEthnicityOrGenderAndPercentOfMajor) {
+        ArrayList<Object> majorsAndPercentages = new ArrayList<>();
+
+        for(int iterator = 1; iterator < raceEthnicityOrGenderAndPercentOfMajor.length; iterator++) {
+            HashMap<String, Double> majorToPercentage = new HashMap<>();
+            majorToPercentage.put(majors[iterator], Double.valueOf(raceEthnicityOrGenderAndPercentOfMajor[iterator]));
+            majorsAndPercentages.add(majorToPercentage);
+        }
+
+        return majorsAndPercentages;
     }
 }
--- src/test/java/harvardWeCode/StudentServiceTest.java
+++ src/test/java/harvardWeCode/StudentServiceTest.java
@@ -2,6 +2,7 @@ package harvardWeCode;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -22,5 +23,49 @@ public class StudentServiceTest {
         StudentService studentService = new StudentService();
         Map majorsByEthnicityRaceAndGender = studentService.list();
         assertThat(majorsByEthnicityRaceAndGender.containsKey("All races and ethnicities"), is(true));
+        assertThat(majorsByEthnicityRaceAndGender.containsKey("Female"), is(false));
+        assertThat(majorsByEthnicityRaceAndGender.containsKey("White"), is(true));
+    }
+
+    @Test
+    public void shouldCreateHashWithRaceAndEthnicityAsKeysAndGenderAsSubKeys() {
+        ArrayList<String[]> exampleData = getExampleCSVData("Asian");
+
+        StudentService studentService = new StudentService();
+        Map majorsByEthnicityRaceAndGender = studentService.createListOfMajorsByPercent(exampleData);
+
+        assertThat(majorsByEthnicityRaceAndGender.containsKey("Female"), is(false));
+
+        String asianKey = "Asian";
+        assertThat(majorsByEthnicityRaceAndGender.containsKey(asianKey), is(true));
+        assertThat(((Map) majorsByEthnicityRaceAndGender.get(asianKey)).containsKey("Female"), is(true));
+    }
+
+    @Test
+    public void shouldCreateListOfPercentagesByMajorForTotalAndEachGender() {
+        ArrayList<String[]> exampleData = getExampleCSVData("Asian");
+        StudentService studentService = new StudentService();
+        Map majorsByEthnicityRaceAndGender = studentService.createListOfMajorsByPercent(exampleData);
+
+        Map mapOfMajorPercentagesForAsianEthnicity = (Map) majorsByEthnicityRaceAndGender.get("Asian");
+        assertThat(((List) mapOfMajorPercentagesForAsianEthnicity.get("Female")).isEmpty(), is(false));
+    }
+
+    private ArrayList<String[]> getExampleCSVData(String ethnicityKey) {
+        String[] row1 = String.format("%s,52.7,20.3,15.0,5.6,2.8,9.0", ethnicityKey).split(",");
+        String[] row2 = "Female,45.4,22.5,7.0,2.9,2.5,10.5".split(",");
+        String[] row3 = "Male,60.0,17.8,23.4,8.4,3.2,7.2".split(",");
+        ArrayList<String[]> exampleData = new ArrayList<>();
+        exampleData.add(row1);
+        exampleData.add(row2);
+        exampleData.add(row3);
+        return exampleData;
+    }
+
+    private List<String[]> getExampleMultipleGroupsCSVData() {
+        ArrayList<String[]> dataSet = new ArrayList<>();
+        dataSet.addAll(getExampleCSVData("Asian"));
+        dataSet.addAll(getExampleCSVData("White"));
+        return dataSet;
     }
 }
